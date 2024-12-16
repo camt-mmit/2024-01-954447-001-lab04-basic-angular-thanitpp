2e80a7cbc193b62ec35a2f4457ac0b2ac7691a4f
p <kaook46@gmail.com>
2024-12-16 14:59:03 +0700

    initial

---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  imports: [],
+  templateUrl: './hello-world.component.html',
+  styleUrl: './hello-world.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HelloWorldComponent {
+
+}
